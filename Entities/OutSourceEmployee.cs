f56b50ae9d70b7cb9ec2435b7a937b346c5a3cf2
Felipe Brandao <brandao154@outlook.com>
2024-01-25 16:40:45 -0300

    Inheritance proj

---
+++ Entities/OutSourceEmployee.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace inheritance.Entities
+{
+    internal class OutSourceEmployee : Employee
+    {
+        public double Additional { get; set; }
+
+        public OutSourceEmployee() { }
+        public OutSourceEmployee(string name, int hours, double valuePerHour, double additional) : base(name, hours, valuePerHour)
+        {
+            Additional = additional;
+        }
+
+        public sealed override double Payment() //SEALED NÃO PODE SER USADA POR OUTRA SUBCLASSE
+                                                //E OVERRIDE POSSO MODIFICAR O MÉTODO DA SUPERCLASSE
+        {
+            return base.Payment() + (Additional + (Additional * 0.10)); //UMA FORMA MAIS SIMPLES DE DECLARAR SERIA (1.1 * ADDITIONAL)
+                                                                        //QUE SERIA OS 110% DO VALOR ADICIONAL
+            
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using inheritance.Entities;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter the number of employees: ");
+        int n = int.Parse(Console.ReadLine());
+        List<Employee> employees = new List<Employee>(); //AQUI A LISTA ESTÁ COMPLETAMENTE DECLARADA E INSTANCIADA.
+
+        for (int i = 1 ; i <= n; i++)
+        {
+            Console.WriteLine($"Employee #{i} data: ");
+            Console.Write("Outsourced (y/n)? ");
+            char yN = char.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Hours: ");
+            int hours = int.Parse(Console.ReadLine());
+            Console.Write("Value per hour: ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (yN == 'y' || yN == 'Y')
+            {
+                Console.Write("Additional charge: ");
+                double adittional = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                employees.Add(new OutSourceEmployee(name, hours, value, adittional)); //LISTAS NÃO FICAM PRESAS DENTRO DE IFS E ELSES
+                                                                                      //A NÃO SER QUE SEJAM INSTANCIADAS DENTRO
+                                                                                      //(DECLARADA E INSTANCIADA NA LINHA 10) 
+            }
+            else
+            {
+                employees.Add(new Employee(name, hours, value)); // ISSO NÃO É UMA LISTA INSTANCIADA DENTRO DA OUTRA, É UM CONSTRUTOR
+                                                                 // SENDO INSTANCIADO DENTRO DA LISTA! (VER ANOTAÇÃO LINHA 10)
+                
+            }           
+
+        }
+
+        Console.WriteLine("PAYMENTS: ");
+
+        foreach (Employee list in employees)
+        {
+            Console.Write(list.Name + " - $");
+            Console.Write(list.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+        }
+
+    }
+}
